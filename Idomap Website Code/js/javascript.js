413c6c0e9732fee065f6dd9d11754587bd7b1882
Ketetha Olengue <kolengue@smu.edu>
2012-11-05 16:19:38 -0600

    added a sign in page :)

---
+++ Idomap Website Code/js/javascript.js
@@ -0,0 +1,48 @@
+$(document).ready(function() {
+      //$('#roundHolder').roundabout();
+      //btnNext: ".next"
+      //autoplay: true
+      
+      $('.roundabout-holder li')
+			.bind({
+				"reposition": function() {
+					var degrees = $(this).data('roundabout').degrees,
+						roundaboutBearing = $(this).parent().data('roundabout').bearing,
+						rotateY = Math.round(roundaboutBearing - degrees);
+					
+					$(this).css({
+						"-webkit-transform": "perspective(2000) rotateY(" + rotateY + "deg)",
+						"-moz-transform": "perspective(2000) rotateY(" + rotateY + "deg)",
+						"transform": "perspective(2000) rotateY(" + rotateY + "deg)"
+					});
+				}
+				
+			//$(this).css('top', '0px');
+			})
+		
+		$('.roundabout-holder').roundabout({
+			minScale: 0.7,
+			easing: 'easeOutExpo',
+			btnStartAutoplay: ".play",
+			btnStopAutoplay: ".stop",
+			btnNext: ".next",
+			btnPrev: ".prev",
+			autoplay: true,
+			duration: 1600
+		});
+});
+
+window.onload = function() {
+
+		$('.play').click( function(){
+			console.log('clicked play');
+			$(this).removeClass('play').addClass('stop');
+			$(this).html("&#9689;");
+		});
+		
+		$('.stop').click( function(){
+			console.log('clicked stop');
+			$(this).removeClass('stop').addClass('play');
+			$(this).html("&#9658;");
+		});
+}
